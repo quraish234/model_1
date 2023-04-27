188f4d605019f461e9fc5f5f133d8683fec2c6f6
quraish234 <129156463+quraish234@users.noreply.github.com>
2023-04-27 17:46:35 +0500

    commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,81 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import {OrbitControls, Environment, Loader} from "@react-three/drei";
+import {Canvas} from "@react-three/fiber";
+//import {OrbitControls} from "@react-three/drei";
+//import Model from './Components/Mode'
+//import Model from './assets/models'
+import Header from "./header";
+import { Suspense } from "react";
+import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
+import { useLoader } from "@react-three/fiber";
 
-function App() {
+/* 
+function Model(props){
+    //const {scene} = useGLTF("/civic.glb");
+    //const {scene} = useGLTF("/shirt.glb");
+    //const {scene} = useGLTF("/quraish.glb");
+    //const {scene} = useGLTF("/bmw.glb");
+    const gltf = useLoader(GLTFLoader, "./untitled.gltf");
+    return <primitive object={scene} {...props} /> 
+  } */
+  const Model = () => {
+    const gltf = useLoader(GLTFLoader, "./untitled.gltf");
+    //const gltf = useLoader(GLTFLoader, "./Poimandres.gltf");
+    return (
+      <>
+        <primitive object={gltf.scene} scale={3} />
+      </>
+    );
+  };
+  
+  export default function App() {
+    return (
+      <div className="App">
+      <Header />
+      <Canvas camera={{ position: [0, 5, 8] }}>
+        
+        <Suspense fallback={null}>
+          <Model />
+          <OrbitControls target={[0, 2.5, 0]} />
+          <Environment preset="apartment" background={false} />
+        </Suspense>
+      </Canvas>
+      <Loader></Loader>
+    </div>
+  );
+  }
+
+/* function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Cube Rotation</h1>
+      <Model/>
     </div>
   );
 }
 
-export default App;
+export default App; */
+
+
+
+
+
+//import React from 'react';
+/* import {Text, View} from 'react-native';
+
+const App = () => {
+  return (
+    <View
+      style={{
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+      }}>
+      <Text>Hello, world!</Text>
+    </View>
+  );
+};
+
+export default App; */
+
---
+++ src/header.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Header() {
+  return (
+    <header>
+      <h1>CyberWear Clothing 3D</h1>
+    </header>
+  );
+}
+
+export default Header;
