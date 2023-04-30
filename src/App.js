d2b71bb950a9f4c8ea0a2bc3e8c2211578022760
quraish234 <129156463+quraish234@users.noreply.github.com>
2023-04-30 17:44:48 +0500

    commit final

--- src/App.js
+++ src/App.js
@@ -5,30 +5,44 @@ import {Canvas} from "@react-three/fiber";
 //import {OrbitControls} from "@react-three/drei";
 //import Model from './Components/Mode'
 //import Model from './assets/models'
+//useGLTF ,
+
+
 import Header from "./header";
 import { Suspense } from "react";
 import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
 import { useLoader } from "@react-three/fiber";
+//import { FBXLoader } from 'three/examples/jsm/loaders/FBXLoader'
 
-/* 
-function Model(props){
+
+/* function Model(props){
+    const {scene} = useGLTF("/fcharacter.glb");
     //const {scene} = useGLTF("/civic.glb");
     //const {scene} = useGLTF("/shirt.glb");
     //const {scene} = useGLTF("/quraish.glb");
     //const {scene} = useGLTF("/bmw.glb");
-    const gltf = useLoader(GLTFLoader, "./untitled.gltf");
+    //const gltf = useLoader(GLTFLoader, "./untitled.gltf");
     return <primitive object={scene} {...props} /> 
   } */
+
   const Model = () => {
-    const gltf = useLoader(GLTFLoader, "./untitled.gltf");
+    //const gltf = useLoader(GLTFLoader, "./Sfm.gltf");
+    //const gltf = useLoader(GLTFLoader, "./Qpose.gltf");
+    const gltf = useLoader(GLTFLoader, "./armdown.gltf");
+    //const gltf = useLoader(GLTFLoader, "./bjacket.gltf");
+    //const gltf = useLoader(GLTFLoader, "./blue.gltf");
+    //const gltf = useLoader(GLTFLoader, "./abaya.gltf");
+    //const gltf = useLoader(GLTFLoader, "./untitled.gltf");
     //const gltf = useLoader(GLTFLoader, "./Poimandres.gltf");
     return (
       <>
-        <primitive object={gltf.scene} scale={3} />
+        <primitive object={gltf.scene} scale={4} />
       </>
     );
   };
   
+
+
   export default function App() {
     return (
       <div className="App">
